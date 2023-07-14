17deee7d3bb0ed13019a83a2b1c64c9223e89ef7
ucabcbo <christian.boehm.22@ucl.ac.uk>
2023-07-14 21:16:49 +0100

    baseline update

--- sched.sh
+++ sched.sh
@@ -4,7 +4,7 @@ file_path="/home/ucabcbo/sis2/experiments/"$1".json"
 
 if test -f "$file_path"; then
     cd "/home/ucabcbo/sis2/"
-    qsub -N $1 -l "h_rt=4:00:0,mem=20G,gpu=1,tmpfs=10G" -m beas train.sh $1
+    qsub -N $1 -l "h_rt=8:00:0,mem=20G,gpu=1,tmpfs=50G" -m beas train.sh $1
 else
     echo "File does not exist: "$file_path
 fi
