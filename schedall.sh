2a1502f682ee4e34b8b5d6e9b2808f5fb057463d
ucabcbo <christian.boehm.22@ucl.ac.uk>
2023-08-06 02:55:28 +0100

    multiplication correction

--- schedall.sh
+++ schedall.sh
@@ -1,105 +1,121 @@
 #!/bin/sh
 
-./sched.sh 8 50 s5lw_inmask/alt12/dmnet
-./sched.sh 8 50 s5lw_inmask/alt12/dsen2w
-./sched.sh 8 50 s5lw_inmask/alt12/pix2pix
-./sched.sh 8 50 s5lw_inmask/alt12/psgan
-./sched.sh 8 50 s5lw_inmask/alt12/sis2
-./sched.sh 8 50 s5lw_inmask/alt12/sis2-1
-./sched.sh 8 50 s5lw_inmask/alt12/sis2-2
-./sched.sh 8 50 s5lw_inmask/alt12/sis2-3
-./sched.sh 8 50 s5lw_inmask/alt12/sis3
-./sched.sh 8 50 s5lw_inmask/alt12/sis3-1
-./sched.sh 8 50 s5lw_inmask/alt12/sis3-2
-./sched.sh 8 50 s5lw_inmask/alt12/sis3-3
-./sched.sh 8 50 s5lw_inmask/alt12/sis3-a
-./sched.sh 8 50 s5lw_inmask/alt12/sis3-b
-./sched.sh 8 50 s5lw_inmask/alt12/sis3-c
-./sched.sh 8 50 s5lw_inmask/alt12/sis3-d
-./sched.sh 8 50 s5lw_inmask/alt12/sis4
-./sched.sh 8 50 s5lw_inmask/alt12/sis5
-./sched.sh 8 50 s5lw_inmask/alt12/sis6
-./sched.sh 8 50 s5lw_inmask/alt12/sis10
-./sched.sh 8 50 s5lw_inmask/alt12/sis11
-./sched.sh 8 50 s5lw_inmask/alt12/sis12
-./sched.sh 8 50 s5lw_inmask/alt12/srgan
-./sched.sh 8 50 s5lw_inmask/alt12/srs3
-./sched.sh 8 100 s5lw_inmask/alt12/tarsgan
+# ./sched.sh 8 50 s5lw_inmask/alt12/dmnet
+# ./sched.sh 8 50 s5lw_inmask/alt12/dsen2w
+# ./sched.sh 8 50 s5lw_inmask/alt12/pix2pix
+# ./sched.sh 8 50 s5lw_inmask/alt12/psgan
+# ./sched.sh 8 50 s5lw_inmask/alt12/sis2
+# ./sched.sh 8 50 s5lw_inmask/alt12/sis2-1
+# ./sched.sh 8 50 s5lw_inmask/alt12/sis2-2
+# ./sched.sh 8 50 s5lw_inmask/alt12/sis2-3
+# ./sched.sh 8 50 s5lw_inmask/alt12/sis3
+# ./sched.sh 8 50 s5lw_inmask/alt12/sis3-1
+# ./sched.sh 8 50 s5lw_inmask/alt12/sis3-2
+# ./sched.sh 8 50 s5lw_inmask/alt12/sis3-3
+# ./sched.sh 8 50 s5lw_inmask/alt12/sis3-a
+# ./sched.sh 8 50 s5lw_inmask/alt12/sis3-b
+# ./sched.sh 8 50 s5lw_inmask/alt12/sis3-c
+# ./sched.sh 8 50 s5lw_inmask/alt12/sis3-d
+# ./sched.sh 8 50 s5lw_inmask/alt12/sis4
+# ./sched.sh 8 50 s5lw_inmask/alt12/sis5
+# ./sched.sh 8 50 s5lw_inmask/alt12/sis6
+# ./sched.sh 8 50 s5lw_inmask/alt12/sis10
+# ./sched.sh 8 50 s5lw_inmask/alt12/sis11
+# ./sched.sh 8 50 s5lw_inmask/alt12/sis12
+# ./sched.sh 8 50 s5lw_inmask/alt12/srgan
+# ./sched.sh 8 50 s5lw_inmask/alt12/srs3
+# ./sched.sh 8 100 s5lw_inmask/alt12/tarsgan
+./sched.sh 8 50 s5lw_inmask/alt12/sis22
+./sched.sh 8 50 s5lw_inmask/alt12/sis23
+./sched.sh 8 50 s5lw_inmask/alt12/sis25
+./sched.sh 8 50 s5lw_inmask/alt12/sis26
 
-./sched.sh 8 50 s5lw_inmask/cur/dmnet
-./sched.sh 8 50 s5lw_inmask/cur/dsen2w
-./sched.sh 8 50 s5lw_inmask/cur/pix2pix
-./sched.sh 8 50 s5lw_inmask/cur/psgan
-./sched.sh 8 50 s5lw_inmask/cur/sis2
-./sched.sh 8 50 s5lw_inmask/cur/sis2-1
-./sched.sh 8 50 s5lw_inmask/cur/sis2-2
-./sched.sh 8 50 s5lw_inmask/cur/sis2-3
-./sched.sh 8 50 s5lw_inmask/cur/sis3
-./sched.sh 8 50 s5lw_inmask/cur/sis3-1
-./sched.sh 8 50 s5lw_inmask/cur/sis3-2
-./sched.sh 8 50 s5lw_inmask/cur/sis3-3
-./sched.sh 8 50 s5lw_inmask/cur/sis3-a
-./sched.sh 8 50 s5lw_inmask/cur/sis3-b
-./sched.sh 8 50 s5lw_inmask/cur/sis3-c
-./sched.sh 8 50 s5lw_inmask/cur/sis3-d
-./sched.sh 8 50 s5lw_inmask/cur/sis4
-./sched.sh 8 50 s5lw_inmask/cur/sis5
-./sched.sh 8 50 s5lw_inmask/cur/sis6
-./sched.sh 8 50 s5lw_inmask/cur/sis10
-./sched.sh 8 50 s5lw_inmask/cur/sis11
-./sched.sh 8 50 s5lw_inmask/cur/sis12
-./sched.sh 8 50 s5lw_inmask/cur/srgan
-./sched.sh 8 50 s5lw_inmask/cur/srs3
-./sched.sh 8 100 s5lw_inmask/cur/tarsgan
+# ./sched.sh 8 50 s5lw_inmask/cur/dmnet
+# ./sched.sh 8 50 s5lw_inmask/cur/dsen2w
+# ./sched.sh 8 50 s5lw_inmask/cur/pix2pix
+# ./sched.sh 8 50 s5lw_inmask/cur/psgan
+# ./sched.sh 8 50 s5lw_inmask/cur/sis2
+# ./sched.sh 8 50 s5lw_inmask/cur/sis2-1
+# ./sched.sh 8 50 s5lw_inmask/cur/sis2-2
+# ./sched.sh 8 50 s5lw_inmask/cur/sis2-3
+# ./sched.sh 8 50 s5lw_inmask/cur/sis3
+# ./sched.sh 8 50 s5lw_inmask/cur/sis3-1
+# ./sched.sh 8 50 s5lw_inmask/cur/sis3-2
+# ./sched.sh 8 50 s5lw_inmask/cur/sis3-3
+# ./sched.sh 8 50 s5lw_inmask/cur/sis3-a
+# ./sched.sh 8 50 s5lw_inmask/cur/sis3-b
+# ./sched.sh 8 50 s5lw_inmask/cur/sis3-c
+# ./sched.sh 8 50 s5lw_inmask/cur/sis3-d
+# ./sched.sh 8 50 s5lw_inmask/cur/sis4
+# ./sched.sh 8 50 s5lw_inmask/cur/sis5
+# ./sched.sh 8 50 s5lw_inmask/cur/sis6
+# ./sched.sh 8 50 s5lw_inmask/cur/sis10
+# ./sched.sh 8 50 s5lw_inmask/cur/sis11
+# ./sched.sh 8 50 s5lw_inmask/cur/sis12
+# ./sched.sh 8 50 s5lw_inmask/cur/srgan
+# ./sched.sh 8 50 s5lw_inmask/cur/srs3
+# ./sched.sh 8 100 s5lw_inmask/cur/tarsgan
+./sched.sh 8 50 s5lw_inmask/cur/sis22
+./sched.sh 8 50 s5lw_inmask/cur/sis23
+./sched.sh 8 50 s5lw_inmask/cur/sis25
+./sched.sh 8 50 s5lw_inmask/cur/sis26
 
-./sched.sh 8 50 s5lw_notinmask/alt12/dmnet
-./sched.sh 8 50 s5lw_notinmask/alt12/dsen2w
-./sched.sh 8 50 s5lw_notinmask/alt12/pix2pix
-./sched.sh 8 50 s5lw_notinmask/alt12/psgan
-./sched.sh 8 50 s5lw_notinmask/alt12/sis2
-./sched.sh 8 50 s5lw_notinmask/alt12/sis2-1
-./sched.sh 8 50 s5lw_notinmask/alt12/sis2-2
-./sched.sh 8 50 s5lw_notinmask/alt12/sis2-3
-./sched.sh 8 50 s5lw_notinmask/alt12/sis3
-./sched.sh 8 50 s5lw_notinmask/alt12/sis3-1
-./sched.sh 8 50 s5lw_notinmask/alt12/sis3-2
-./sched.sh 8 50 s5lw_notinmask/alt12/sis3-3
-./sched.sh 8 50 s5lw_notinmask/alt12/sis3-a
-./sched.sh 8 50 s5lw_notinmask/alt12/sis3-b
-./sched.sh 8 50 s5lw_notinmask/alt12/sis3-c
-./sched.sh 8 50 s5lw_notinmask/alt12/sis3-d
-./sched.sh 8 50 s5lw_notinmask/alt12/sis4
-./sched.sh 8 50 s5lw_notinmask/alt12/sis5
-./sched.sh 8 50 s5lw_notinmask/alt12/sis6
-./sched.sh 8 50 s5lw_notinmask/alt12/sis10
-./sched.sh 8 50 s5lw_notinmask/alt12/sis11
-./sched.sh 8 50 s5lw_notinmask/alt12/sis12
-./sched.sh 8 50 s5lw_notinmask/alt12/srgan
-./sched.sh 8 50 s5lw_notinmask/alt12/srs3
-./sched.sh 8 100 s5lw_notinmask/alt12/tarsgan
+# ./sched.sh 8 50 s5lw_notinmask/alt12/dmnet
+# ./sched.sh 8 50 s5lw_notinmask/alt12/dsen2w
+# ./sched.sh 8 50 s5lw_notinmask/alt12/pix2pix
+# ./sched.sh 8 50 s5lw_notinmask/alt12/psgan
+# ./sched.sh 8 50 s5lw_notinmask/alt12/sis2
+# ./sched.sh 8 50 s5lw_notinmask/alt12/sis2-1
+# ./sched.sh 8 50 s5lw_notinmask/alt12/sis2-2
+# ./sched.sh 8 50 s5lw_notinmask/alt12/sis2-3
+# ./sched.sh 8 50 s5lw_notinmask/alt12/sis3
+# ./sched.sh 8 50 s5lw_notinmask/alt12/sis3-1
+# ./sched.sh 8 50 s5lw_notinmask/alt12/sis3-2
+# ./sched.sh 8 50 s5lw_notinmask/alt12/sis3-3
+# ./sched.sh 8 50 s5lw_notinmask/alt12/sis3-a
+# ./sched.sh 8 50 s5lw_notinmask/alt12/sis3-b
+# ./sched.sh 8 50 s5lw_notinmask/alt12/sis3-c
+# ./sched.sh 8 50 s5lw_notinmask/alt12/sis3-d
+# ./sched.sh 8 50 s5lw_notinmask/alt12/sis4
+# ./sched.sh 8 50 s5lw_notinmask/alt12/sis5
+# ./sched.sh 8 50 s5lw_notinmask/alt12/sis6
+# ./sched.sh 8 50 s5lw_notinmask/alt12/sis10
+# ./sched.sh 8 50 s5lw_notinmask/alt12/sis11
+# ./sched.sh 8 50 s5lw_notinmask/alt12/sis12
+# ./sched.sh 8 50 s5lw_notinmask/alt12/srgan
+# ./sched.sh 8 50 s5lw_notinmask/alt12/srs3
+# ./sched.sh 8 100 s5lw_notinmask/alt12/tarsgan
+./sched.sh 8 50 s5lw_notinmask/alt12/sis22
+./sched.sh 8 50 s5lw_notinmask/alt12/sis23
+./sched.sh 8 50 s5lw_notinmask/alt12/sis25
+./sched.sh 8 50 s5lw_notinmask/alt12/sis26
 
-./sched.sh 8 50 s5lw_notinmask/cur/dmnet
-./sched.sh 8 50 s5lw_notinmask/cur/dsen2w
-./sched.sh 8 50 s5lw_notinmask/cur/pix2pix
-./sched.sh 8 50 s5lw_notinmask/cur/psgan
-./sched.sh 8 50 s5lw_notinmask/cur/sis2
-./sched.sh 8 50 s5lw_notinmask/cur/sis2-1
-./sched.sh 8 50 s5lw_notinmask/cur/sis2-2
-./sched.sh 8 50 s5lw_notinmask/cur/sis2-3
-./sched.sh 8 50 s5lw_notinmask/cur/sis3
-./sched.sh 8 50 s5lw_notinmask/cur/sis3-1
-./sched.sh 8 50 s5lw_notinmask/cur/sis3-2
-./sched.sh 8 50 s5lw_notinmask/cur/sis3-3
-./sched.sh 8 50 s5lw_notinmask/cur/sis3-a
-./sched.sh 8 50 s5lw_notinmask/cur/sis3-b
-./sched.sh 8 50 s5lw_notinmask/cur/sis3-c
-./sched.sh 8 50 s5lw_notinmask/cur/sis3-d
-./sched.sh 8 50 s5lw_notinmask/cur/sis4
-./sched.sh 8 50 s5lw_notinmask/cur/sis5
-./sched.sh 8 50 s5lw_notinmask/cur/sis6
-./sched.sh 8 50 s5lw_notinmask/cur/sis10
-./sched.sh 8 50 s5lw_notinmask/cur/sis11
-./sched.sh 8 50 s5lw_notinmask/cur/sis12
-./sched.sh 8 50 s5lw_notinmask/cur/srgan
-./sched.sh 8 50 s5lw_notinmask/cur/srs3
-./sched.sh 8 100 s5lw_notinmask/cur/tarsgan
+# ./sched.sh 8 50 s5lw_notinmask/cur/dmnet
+# ./sched.sh 8 50 s5lw_notinmask/cur/dsen2w
+# ./sched.sh 8 50 s5lw_notinmask/cur/pix2pix
+# ./sched.sh 8 50 s5lw_notinmask/cur/psgan
+# ./sched.sh 8 50 s5lw_notinmask/cur/sis2
+# ./sched.sh 8 50 s5lw_notinmask/cur/sis2-1
+# ./sched.sh 8 50 s5lw_notinmask/cur/sis2-2
+# ./sched.sh 8 50 s5lw_notinmask/cur/sis2-3
+# ./sched.sh 8 50 s5lw_notinmask/cur/sis3
+# ./sched.sh 8 50 s5lw_notinmask/cur/sis3-1
+# ./sched.sh 8 50 s5lw_notinmask/cur/sis3-2
+# ./sched.sh 8 50 s5lw_notinmask/cur/sis3-3
+# ./sched.sh 8 50 s5lw_notinmask/cur/sis3-a
+# ./sched.sh 8 50 s5lw_notinmask/cur/sis3-b
+# ./sched.sh 8 50 s5lw_notinmask/cur/sis3-c
+# ./sched.sh 8 50 s5lw_notinmask/cur/sis3-d
+# ./sched.sh 8 50 s5lw_notinmask/cur/sis4
+# ./sched.sh 8 50 s5lw_notinmask/cur/sis5
+# ./sched.sh 8 50 s5lw_notinmask/cur/sis6
+# ./sched.sh 8 50 s5lw_notinmask/cur/sis10
+# ./sched.sh 8 50 s5lw_notinmask/cur/sis11
+# ./sched.sh 8 50 s5lw_notinmask/cur/sis12
+# ./sched.sh 8 50 s5lw_notinmask/cur/srgan
+# ./sched.sh 8 50 s5lw_notinmask/cur/srs3
+# ./sched.sh 8 100 s5lw_notinmask/cur/tarsgan
+./sched.sh 8 50 s5lw_notinmask/cur/sis22
+./sched.sh 8 50 s5lw_notinmask/cur/sis23
+./sched.sh 8 50 s5lw_notinmask/cur/sis25
+./sched.sh 8 50 s5lw_notinmask/cur/sis26
